92f4318d33846a4bc257f2220602044d050a8696
Chris Ardon <rockrisbl@gmail.com>
2021-10-20 07:47:40 -0600

    Markers.js agregado

---
+++ Markers.js
@@ -0,0 +1,38 @@
+export const markers = [
+    {
+        coordinate:{
+            latitude: 13.4945688,
+            longitude: -89.3868631,
+        },
+        title: 'Playa el Tunco',
+        description: 'Playa el Tunco en la Libertad',
+        image: 'https://lh3.googleusercontent.com/p/AF1QipPM0zAupIxVZe7R6YSgPpt-ff-LAATrxS2yN70i=s1600-h1836',
+    },
+    {
+        coordinate:{
+            latitude: 13.1698526,
+            longitude: -88.3076309,
+        },
+        title: 'Playa el Espino',
+        description: 'Playa el Espino Usulutan',
+        image: 'https://cdn-pro.elsalvador.com/wp-content/uploads/2018/01/El-Espino.jpg',
+    },
+    {
+        coordinate:{
+            latitude: 13.4924991,
+            longitude: -89.409454,
+        },
+        title: 'Playa el Sunzal',
+        description: 'Playa el Sunzal La Libertad',
+        image: 'https://wikisivar.com/wp-content/uploads/2018/12/Surfistas-en-playa-en-Sunzal.jpg',
+    },
+    {
+        coordinate:{
+            latitude: 13.5249991,
+            longitude: -89.8141763,
+        },
+        title: 'Playa los Cóbanos',
+        description: 'Playa los cóbanos Sonsonate',
+        image: 'https://lh4.googleusercontent.com/-Qv6F9BgYDD0/U7roPAjpORI/AAAAAAAAFag/DeiU57o0X2o/s460/playa-los-cobanos.jpg',
+    },
+];
